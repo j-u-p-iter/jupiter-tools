b39850d1965bae4d5be75fd768f2e8e90416c8ae
1234 <a1234@1234s-MacBook-Pro.local>
2023-11-11 12:05:52 +0100

    fix(jupiter-tools): adds some logs

--- src/setUpTsConfigs.ts
+++ src/setUpTsConfigs.ts
@@ -38,6 +38,10 @@ export const setUpTsConfigs = () => {
 
   const pathToBuildTsConfig = getPathToBuildTsConfig();
 
+  console.log("pathToBaseTsConfig:", pathToBaseTsConfig);
+
+  console.log("pathToBuildTsConfig:", pathToBuildTsConfig);
+
   /**
    * If config was already set up
    *   return from method.
